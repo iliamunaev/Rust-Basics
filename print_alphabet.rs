65863b6e31fbaa987655e3416334d81b12ff1ad5
Ilia Munaev <ilyamunaev@gmail.com>
2025-03-31 23:10:40 +0300

    Refactor: print_alphabet.rs

--- print_alphabet.rs
+++ print_alphabet.rs
@@ -5,7 +5,6 @@ use std::io::{self, Write};
 /// This function locks the stdout for exclusive access during the write operation,
 /// writes the given character to the terminal, and then flushes the output to ensure
 /// immediate display. It returns an `io::Result<()>`, indicating success or failure.
-///
 fn rust_putchar(c: char) -> Result<(), std::io::Error> {
     let stdout = io::stdout();
     let mut handle = stdout.lock();
@@ -20,16 +19,11 @@ fn rust_putchar(c: char) -> Result<(), std::io::Error> {
 ///
 /// This function prints all lowercase English letters from 'a' to 'z',
 /// followed by a newline character.
-///
-/// # Returns
-///
-/// * `Ok(())` if all characters are successfully printed.
-/// * `Err(std::io::Error)` if an I/O error occurs during the printing process.
 fn main() -> Result<(), std::io::Error> {
     for letter in 'a'..='z' {
-        let _ = rust_putchar(letter);
+        rust_putchar(letter)?;
     }
-    let _ = rust_putchar('\n');
+    rust_putchar('\n')?;
 
     Ok(())
 }
