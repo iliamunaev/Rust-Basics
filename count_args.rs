6a7e2c280cdbeb6610d605c972ca137a886fa74c
Ilia Munaev <ilyamunaev@gmail.com>
2025-03-31 23:11:45 +0300

    Add: count_args function

--- count_args.rs
+++ count_args.rs
@@ -1,12 +1,10 @@
-use std::env;
-
 /// Counts the number of command-line arguments passed to the program.
 ///
 /// This function iterates over all the command-line arguments provided to the program
 /// and returns their count as a `usize`. Note that the program name is also considered
 /// an argument, so the count will always be at least one.
 fn count_args() -> usize {
-    let args = env::args();
+    let args = std::env::args();
     let mut count = 0;
 
     for _ in args {
@@ -16,7 +14,18 @@ fn count_args() -> usize {
     count
 }
 
-fn main() {
+/// The main function of the program.
+///
+/// This function retrieves the count of command-line arguments using the `count_args()` function,
+/// prints the count to the standard output, and returns an `io::Result<()>` indicating success or failure.
+///
+/// # Returns
+///
+/// * `Ok(())` - If the count is printed successfully.
+/// * `Err(std::io::Error)` - If an error occurs during printing.
+fn main() -> Result<(), std::io::Error> {
     let arg_count = count_args();
     println!("Number of arguments: {}", arg_count);
+
+    Ok(())
 }
