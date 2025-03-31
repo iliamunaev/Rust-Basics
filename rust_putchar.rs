f9ac22a2a012e3d4247db7a586638db54e656c85
Ilia Munaev <ilyamunaev@gmail.com>
2025-03-31 22:56:00 +0300

    Refactor: open Result

---
+++ rust_putchar.rs
@@ -0,0 +1,20 @@
+// https://doc.rust-lang.org/std/macro.write.html
+
+use std::io::{self, Write};
+
+pub fn rust_putchar(c: char) -> Result<(), std::io::Error> {
+    let stdout = io::stdout();
+    let mut handle = stdout.lock();
+
+    write!(handle, "{}", c)?;
+    handle.flush()?;
+
+    Ok(())
+}
+
+// // test
+// fn main() -> io::Result<()> {
+//     rust_putchar('A')?;
+//     rust_putchar('\n')?;
+//     Ok(())
+// }
